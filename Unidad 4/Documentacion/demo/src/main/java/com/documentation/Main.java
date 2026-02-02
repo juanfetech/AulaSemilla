9730d7fe281c6568e72e010ce1730c303550bbf1
JuanFe Dev Master <professional.dev.group@gmail.com>
2026-02-01 21:57:34 -0500

    Demo para Documentación

---
+++ Unidad 4/Documentacion/demo/src/main/java/com/documentation/Main.java
@@ -0,0 +1,18 @@
+package com.documentation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumar(2,4));
+    }
+
+    /**
+     * Calcula la suma de dos números enteros.
+     * 
+     * @param a El primer número.
+     * @param b El segundo número.
+     * @return La suma de a y b.
+     */
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
